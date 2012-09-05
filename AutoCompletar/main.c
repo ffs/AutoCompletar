9adf4dd6fb914df2e76cde73c4db741acfe54f4b
Rodrigo <rodrigoxbr@gmail.com>
2012-09-05 00:38:00 -0300

    Le as palavras do arquivo e armazena na arvore TRIE.

--- AutoCompletar/main.c
+++ AutoCompletar/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "trie.h"
 
 int main(int argc, char *argv[]) {
+	FILE *dicionario = fopen("dicionario", "r");
+	if (!dicionario) {
+		printf("Erro ao abrir o dicionario de palavras!\n");
+		return EXIT_FAILURE;
+	}
 
-	return 1;
+	Trie *trie = trie_new();
+
+	while (!feof(dicionario)) {
+		char *palavra = malloc(64 * sizeof(char));
+		fscanf(dicionario, "%s", palavra);
+
+		trie_insert(trie, palavra, palavra);
+	}
+
+	return EXIT_SUCCESS;
 }
